7d3a146fcaf50ce8f91cd166b49b752ebfe63721
Soumyabrata Ghosh <soghosh@clarku.edu>
2025-02-17 18:29:32 -0500

    Update firebase.js

--- bonding-app/src/lib/firebase.js
+++ bonding-app/src/lib/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,6 +22,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-// const analytics = getAnalytics(app);
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { getAnalytics } from "firebase/analytics";
+const analytics = getAnalytics(app);
+/* eslint-enable @typescript-eslint/no-unused-vars */
 
 export { auth,db }
